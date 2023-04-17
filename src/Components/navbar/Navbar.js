b194a10a8806719bc64c63b2bda08d12a7964f9a
abhi9720 <abhishektiwari9720@gmail.com>
2023-04-17 23:21:08 +0530

    remove dark mode

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -16,12 +16,12 @@ function Navbar() {
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/" className="flex items-center">
                     <img src="../../assets/logo.jpeg" className="h-8 mr-3" alt="Qubit Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">QuBit</span>
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap">QuBit</span>
                 </a>
                 <button
                     data-collapse-toggle="navbar-default"
                     type="button"
-                    className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200"
                     aria-controls="navbar-default"
                     aria-expanded={isOpen}
                     onClick={toggleNavbar}
@@ -39,7 +39,7 @@ function Navbar() {
                     )}
                 </button>
                 <div className={`w-full md:block md:w-auto ${isOpen ? '' : 'hidden'}`} id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
                         <li>
                             <NavLink onClick={closeNavbar} to="/" className="block py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 active:bg-blue-500" aria-current="page">Home</NavLink>
                         </li>
